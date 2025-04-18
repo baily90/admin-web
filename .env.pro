c07edfb1a348bafb54e24163577cba0d373bb892
张燕龙 <baily.zhang@soundws.com>
2025-04-18 15:19:56 +0800

    feat(*): 删除业务模块(iot/ai/erp/crm/mall/member/mp)

--- .env.pro
+++ .env.pro
@@ -7,8 +7,5 @@ VITE_ENV=production
 # 请求路径
 VITE_BASE_URL='https://health-gateway.weicha88.com'
 
-# 商城H5会员端域名
-VITE_MALL_H5_DOMAIN='http://mall.yudao.iocoder.cn'
-
 # GoView域名
 VITE_GOVIEW_URL='http://127.0.0.1:3000'
--- src/views/Login/components/LoginForm.vue
+++ src/views/Login/components/LoginForm.vue
@@ -89,7 +89,8 @@
         mode="pop"
         @success="handleLogin"
       />
-      <el-col :span="24" style="padding-right: 10px; padding-left: 10px">
+      
+      <!-- <el-col :span="24" style="padding-right: 10px; padding-left: 10px">
         <el-form-item>
           <el-row :gutter="5" justify="space-between" style="width: 100%">
             <el-col :span="8">
@@ -115,8 +116,9 @@
             </el-col>
           </el-row>
         </el-form-item>
-      </el-col>
-      <el-divider content-position="center">{{ t('login.otherLogin') }}</el-divider>
+      </el-col> -->
+
+      <!-- <el-divider content-position="center">{{ t('login.otherLogin') }}</el-divider>
       <el-col :span="24" style="padding-right: 10px; padding-left: 10px">
         <el-form-item>
           <div class="w-[100%] flex justify-between">
@@ -131,22 +133,7 @@
             />
           </div>
         </el-form-item>
-      </el-col>
-      <el-divider content-position="center">萌新必读</el-divider>
-      <el-col :span="24" style="padding-right: 10px; padding-left: 10px">
-        <el-form-item>
-          <div class="w-[100%] flex justify-between">
-            <el-link href="https://doc.iocoder.cn/" target="_blank">📚开发指南</el-link>
-            <el-link href="https://doc.iocoder.cn/video/" target="_blank">🔥视频教程</el-link>
-            <el-link href="https://www.iocoder.cn/Interview/good-collection/" target="_blank">
-              ⚡面试手册
-            </el-link>
-            <el-link href="http://static.yudao.iocoder.cn/mp/Aix9975.jpeg" target="_blank">
-              🤝外包咨询
-            </el-link>
-          </div>
-        </el-form-item>
-      </el-col>
+      </el-col> -->
     </el-row>
   </el-form>
 </template>
